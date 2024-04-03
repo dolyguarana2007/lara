397ca61d1f029ae5f3b27eda0f8eac6eea3d4777
dolyguarana2007 <128715869+dolyguarana2007@users.noreply.github.com>
2024-04-03 08:44:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,9 +14,21 @@ for(let i=0;i <botoes.length;i++){
     }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2029-10-10-T00:00:00");
-const tempoObjetivo1 = new Date("2025-01-12-T00:00:00");
-const tempoObjetivo1 = new Date("2029-10-10-T00:00:00");
-const tempoObjetivo1 = new Date("2029-10-10-T00:00:00"
-let tempoAtual= new Date();
+const tempos = [tempoObjetivo1];
 
-contadores[0].textContent = yempoObjetivo1-tempoAtual;
+contadores[0].textContent = calculaTempo(tempos[0]);
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
