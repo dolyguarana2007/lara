313c55516d8ef1518201a9e2204cf1116371a9d2
dolyguarana2007 <128715869+dolyguarana2007@users.noreply.github.com>
2024-04-03 08:33:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2029-10-10-T00:00:00");
 const tempoObjetivo1 = new Date("2025-01-12-T00:00:00");
 const tempoObjetivo1 = new Date("2029-10-10-T00:00:00");
-const tempoObjetivo1 = new Date("2029-10-10-T00:00:00");
+const tempoObjetivo1 = new Date("2029-10-10-T00:00:00"
 let tempoAtual= new Date();
 
 contadores[0].textContent = yempoObjetivo1-tempoAtual;
